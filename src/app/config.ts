509170007e1556ae9fe503fab4d3ee652d256cf4
Connor Ruggles <conruggles@gmail.com>
2019-08-22 22:42:30 -0500

    fix startup and update node type package

--- src/app/config.ts
+++ src/app/config.ts
@@ -1,3 +1,3 @@
 export class Config {
-    public static baseUrl = process.env.NODE_ENV !== 'production' ? 'http://localhost:4000/api' : process.env.API_URL;
+    public static baseUrl = (process.env.NODE_ENV !== 'production' ? 'http://localhost:4000' : process.env.API_URL) + '/api';
 }
