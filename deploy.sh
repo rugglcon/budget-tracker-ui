fbc2ef031c4fc7c48ff67d37e8d26134f5da7e99
Connor Ruggles <conruggles@gmail.com>
2020-03-19 22:50:47 -0500

    add deploy script, nvmrc

---
+++ deploy.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+npm ci
+
+npm run build:prod
+
+status=$?
+if [ $status -ne 0 ]; then
+    echo "Something went wrong building for prod"
+    exit 1
+fi
+
+/bin/cp -R dist/budget-tracker/* /var/www/budget-tracker-ui
