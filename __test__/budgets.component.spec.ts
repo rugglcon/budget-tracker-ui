daddb46e8b3cc1665cf440fa577af5d2a95bdc06
Connor Ruggles <conruggles@gmail.com>
2020-03-22 16:49:57 -0500

    get login test running

--- __test__/budgets.component.spec.ts
+++ __test__/budgets.component.spec.ts
@@ -1,6 +1,14 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { BudgetsComponent } from '../src/app/components';
+import { RouterTestingModule } from '@angular/router/testing';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
+import { BudgetService } from 'src/app/services/budget.service';
+import { BudgetResource } from 'src/app/resources/budget.resource';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { ExpenseCache } from 'src/app/cache/expense.cache';
+import { AuthService } from 'src/app/services/auth.service';
+import { AuthResource } from 'src/app/resources/auth.resource';
 
 describe('BudgetsComponent', () => {
   let component: BudgetsComponent;
@@ -8,7 +16,20 @@ describe('BudgetsComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ BudgetsComponent ]
+      declarations: [ BudgetsComponent ],
+      imports: [
+        NgbAlertModule,
+        RouterTestingModule.withRoutes([])
+      ],
+      providers: [
+        BudgetService,
+        BudgetResource,
+        HttpClient,
+        HttpHandler,
+        ExpenseCache,
+        AuthService,
+        AuthResource
+      ]
     })
     .compileComponents();
   }));
--- __test__/login.component.spec.ts
+++ __test__/login.component.spec.ts
@@ -1,14 +1,53 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
 
-import { LoginComponent } from '../src/app/components';
+import { LoginComponent, BudgetsComponent } from '../src/app/components';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+import { HttpClient, HttpHandler } from '@angular/common/http';
+import { AuthResource } from 'src/app/resources/auth.resource';
+import { RouterTestingModule } from '@angular/router/testing';
+import { BudgetService } from 'src/app/services/budget.service';
+import { BudgetResource } from 'src/app/resources/budget.resource';
+import { ExpenseCache } from 'src/app/cache/expense.cache';
+import { Type } from '@angular/core';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { AuthGuard } from 'src/app/guards/auth.guard';
+import { NgbAlertModule } from '@ng-bootstrap/ng-bootstrap';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
+  let budgetService: BudgetService;
+  let authService: AuthService;
+  let router: Router;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      declarations: [
+        LoginComponent,
+        BudgetsComponent
+      ],
+      imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        NgbAlertModule,
+        RouterTestingModule.withRoutes(
+          [
+            { path: 'budgets', component: BudgetsComponent, canActivate: [AuthGuard] },
+            { path: 'login', component: LoginComponent }
+          ]
+        )
+      ],
+      providers: [
+        AuthService,
+        HttpClient,
+        HttpHandler,
+        AuthResource,
+        BudgetService,
+        BudgetResource,
+        ExpenseCache
+      ]
     })
     .compileComponents();
   }));
@@ -17,9 +56,27 @@ describe('LoginComponent', () => {
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    budgetService = fixture.debugElement.injector.get<BudgetService>(BudgetService as Type<BudgetService>);
+    authService = fixture.debugElement.injector.get<AuthService>(AuthService as Type<AuthService>);
+    router = fixture.debugElement.injector.get<Router>(Router as Type<Router>);
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should login and request budgets when form submitted', fakeAsync(() => {
+    jest.spyOn(router, 'navigateByUrl');
+    jest.spyOn(authService, 'login').mockImplementation((creds) => Promise.resolve(true));
+    jest.spyOn(budgetService, 'getAll').mockImplementation(() => Promise.resolve());
+
+    component.username.setValue('user');
+    component.password.setValue('pass');
+
+    component.login();
+    tick();
+
+    expect(router.navigateByUrl).toHaveBeenCalledWith('/budgets');
+  }));
 });
